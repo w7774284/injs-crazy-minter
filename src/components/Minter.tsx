c78918af7e07099735d0d5791bb5edc326c5c257
w7774284 <52136134+w7774284@users.noreply.github.com>
2023-12-19 20:35:04 +0800

    Update Minter.tsx

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -102,7 +102,7 @@ const Minter: React.FC = () => {
       setLogs((pre) => [...pre, `成功导入钱包: ${address.address}`]);
 
       const queryClient = await getQueryClient(
-        "https://sentry.lcd.injective.network:443"
+        "https://1rpc.io/2EAKzDRgGuRCC638Z/inj-rpc"
       );
       const result = await queryClient.cosmos.bank.v1beta1.balance({
         address: address.address,
